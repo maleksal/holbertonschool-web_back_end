da6a9a6fca54eea503aa13f07340531e8ff690e0
maleksal <malek.salem.14@gmail.com>
2021-03-21 20:58:38 +0100

    fix logic

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,4 +6,6 @@ import random
 
 async def wait_random(max_delay=10):
     """waits for a random delay between 0 and max_delay"""
-    return await asyncio.sleep(random.randint(0, max_delay))
+    r = random.random() * max_delay
+    await asyncio.sleep()
+    return r
