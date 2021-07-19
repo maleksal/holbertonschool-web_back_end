1f20ffbad7bbbc8b539dd6a899c2136308a4ce51
maleksal <malek.salem.14@gmail.com>
2021-07-19 21:12:23 +0100

    add task

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""TASK 11 Module"""
+from typing import Union, TypeVar, Any, Mapping
+
+
+typ = TypeVar["T"]
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[typ, None])\
+        -> Union[Any, typ]:
+    """ function safely_get_value """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
