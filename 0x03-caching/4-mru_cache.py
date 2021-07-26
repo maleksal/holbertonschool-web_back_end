f3586b57494bcf76810d42238ffd52bf99bd9aee
maleksal <malek.salem.14@gmail.com>
2021-07-26 20:38:49 +0100

    add

---
+++ 0x03-caching/4-mru_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" LRU Caching modules
+"""
+
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ Cache system, inherts from BaseCaching
+    """
+
+    def __init__(self):
+        """Initializing"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Add item in the cache using FIFO
+        """
+
+        if key and item:
+
+            self.cache_data[key] = item
+
+        if len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS:
+
+            ritem = list(self.cache_data.popitem(last=False))[0]
+
+            print("DISCARD:", ritem)
+
+            self.cache_data.move_to_end(key, last=False)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+
+        if key in self.cache_data.keys():
+
+            self.cache_data.move_to_end(key, last=False)
+
+            return self.cache_data[key]
+
+        return None
