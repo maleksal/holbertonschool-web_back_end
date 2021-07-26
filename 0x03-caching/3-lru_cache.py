1189b9be3a99d7a48734e9d4b717fd1a58b3bd67
maleksal <malek.salem.14@gmail.com>
2021-07-26 20:22:32 +0100

    add

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" LRU Caching modules
+"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ Cache system, inherts from BaseCaching
+    """
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add item in the cache using FIFO
+        """
+        keyss = self.cache_data.keys()
+
+        if key and item:
+
+            if key in keyss:
+                self.cache_data.pop(key)
+
+            elif len(self.cache_data.items()) >= self.MAX_ITEMS and \
+                    key not in keyss:
+
+                item = list(keyss)[0]
+
+                self.cache_data.pop(item)
+
+                print("DISCARD: {}".format(item))
+
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+
+        if key is None:
+
+            return None
+
+        itemKeys = self.cache_data.keys()
+
+        if key in itemKeys:
+
+            value = self.cache_data.pop(key)
+
+            self.cache_data[key] = value
+
+        return self.cache_data.get(key, None)
