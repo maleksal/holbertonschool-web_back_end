7653b7a176b11cd326c9edb575c44ac7979138c0
maleksal <malek.salem.14@gmail.com>
2021-07-28 14:53:36 +0100

    add

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Simple pagination module.
+"""
+
+import csv
+import math
+from os import EX_NOTFOUND
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Return a tuple of size two containing a start index and an end index.
+    """
+    s, e = page * page_size, page * page_size
+    return (s - page_size, e)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        takes two arguments page with default value 1 and page_size with 10.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 1 and page_size > 1
+
+        # start, end indexes
+        srange, erange = index_range(page, page_size)
+
+        dataset = self.dataset()
+
+        if srange <= len(dataset) and erange <= len(dataset):
+            return dataset[srange: erange + 1]
+        return []
