d8a7eb79c696a3dafee220ecb7278b42d33fab11
maleksal <malek.salem.14@gmail.com>
2021-08-07 17:55:20 +0100

    add

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -28,7 +28,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
@@ -38,3 +38,9 @@ class RedactingFormatter(logging.Formatter):
                             self.REDACTION,
                             logging.Formatter(self.FORMAT).format(record),
                             self.SEPARATOR)
+
+
+message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
+log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
+formatter = RedactingFormatter(fields=("email", "ssn", "password"))
+print(formatter.format(log_record))
