8a5b6bf75429fd0bfdd26ac65b3c9ed61955d3ee
maleksal <malek.salem.14@gmail.com>
2021-08-07 17:35:44 +0100

    update code

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -7,10 +7,12 @@ import re
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, sepereator: str) -> str:
+                 message: str, separator: str) -> str:
     """ Returns the log message obfuscated.
     """
-    return f"{sepereator}".join(re.sub(j.split("=")[1], redaction, j)
-                                if j.split('=')[0] in fields else j
-                                for j in message.split(sepereator)
-                                )
+    [
+        re.sub(rf"{field}=.*?{separator}",
+               f"{field}={redaction}{separator}", message)
+        for field in fields
+    ]
+    return message
