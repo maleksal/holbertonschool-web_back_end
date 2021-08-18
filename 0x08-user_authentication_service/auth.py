0c35134a744df95e3b6aaf467216c3e88f65e59a
malek salem <malek.salem.14@gmail.com>
2021-08-18 20:53:06 +0100

    fix

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -55,10 +55,9 @@ class Auth:
         """Takes an email string argument and returns the session ID."""
         try:
             user = self._db.find_user_by(email=email)
-            session_id = _generate_uuid()
-            self._db.update_user(user.id, session_id=session_id)
-
-            return session_id
-
         except NoResultFound:
             return None
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+
+        return session_id
