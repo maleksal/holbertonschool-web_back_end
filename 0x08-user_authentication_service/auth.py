4e3a2920872a6a1cd29bb999a611472127e77e47
malek salem <malek.salem.14@gmail.com>
2021-08-18 20:56:15 +0100

    add new method

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -61,3 +61,12 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
 
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """If the session ID is None or no user is found, return None.
+        Otherwise return the corresponding user."""
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
