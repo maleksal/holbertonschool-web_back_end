51a421db5d06a4bc4de94600b0f8c0a5dd5a7a05
malek salem <malek.salem.14@gmail.com>
2021-08-18 20:25:40 +0100

    add add user endpoint

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """route module for API"""
 
 from auth import Auth
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
@@ -14,6 +14,23 @@ AUTH = Auth()
 def hi():
     """root route"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """implements POST /users route."""
+    email, password = (
+        request.form.get('email'),
+        request.form.get('password')
+    )
+
+    try:
+        check_user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})\
+            if check_user else None
+
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
