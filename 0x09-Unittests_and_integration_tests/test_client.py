7cf396d317ccd1375858c1ada8a1d5492590fc0f
malek salem <malek.salem.14@gmail.com>
2021-08-24 23:29:47 +0100

    add

---
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" test client module """
+
+from client import GithubOrgClient
+from parameterized import parameterized
+import unittest
+from unittest.mock import patch
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """TestGithubOrgClient class."""
+
+    @parameterized.expand([
+        ("google", {}),
+        ("abc", {})
+        ])
+    @patch("client.get_json")
+    def test_org(self, url, result, mock):
+        """test GithubOrgClient.org."""
+
+        mock.return_value = {}
+
+        a_class = GithubOrgClient(url)
+
+        self.assertEqual(a_class.org, result)
+
+        mock.assert_called_once()
+
+
+if __name__ == '__main__':
+    unittest.main()
