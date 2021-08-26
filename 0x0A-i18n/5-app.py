c113d9b2d0c639fedee0fa222858bc44fa374b40
malek salem <malek.salem.14@gmail.com>
2021-08-26 19:52:12 +0100

    add

---
+++ 0x0A-i18n/5-app.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""myroute module"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config(object):
+    """Configurations for Babel object """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def hello():
+    """render html file"""
+    return render_template('4-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """determine language."""
+    locale = request.args.get("locale")
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user():
+    '''Returns user from'''
+    try:
+        return users[int(request.args.get('login_as'))]
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    '''set a user.'''
+    g.user = get_user()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
