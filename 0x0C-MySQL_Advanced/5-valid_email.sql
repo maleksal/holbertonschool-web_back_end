d02ce3bbac66d844acfac0acb60d38ae3a847258
malek salem <malek.salem.14@gmail.com>
2021-09-02 23:20:27 +0100

    add

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+
+--Creates a trigger
+DELIMITER $$
+CREATE TRIGGER updated_email
+BEFORE UPDATE ON users FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+	    SET NEW.valid_email = 0;
+    END IF;
+END$$
+DELIMITER ;
