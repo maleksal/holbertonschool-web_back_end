643fad2d25ff886244d33b38a12605fb6a30eeb0
malek salem <malek.salem.14@gmail.com>
2021-09-02 23:08:41 +0100

    fix

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,2 @@
 -- list all bands and rank them
-SELECT (
-    band_name,
-    (COALESCE(split, 2020) - formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
-)
+SELECT band_name, (COALESCE(split, 2020) - formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
