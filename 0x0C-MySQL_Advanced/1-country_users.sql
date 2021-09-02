47d34c1e0de2ea6428cc03ccb6d7b54a1b54cb93
malek salem <malek.salem.14@gmail.com>
2021-09-02 23:12:08 +0100

    add

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- creates a tables users
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` varchar(255),
+    `country` ENUM('US', 'CO', 'TN') NOT NULL
+);
