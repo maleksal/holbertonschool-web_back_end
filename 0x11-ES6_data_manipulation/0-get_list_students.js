f6ddacd687de7dc937eefaabe397018fd17c297f
malek salem <malek.salem.14@gmail.com>
2021-09-21 21:14:25 +0100

    fix

--- 0x11-ES6_data_manipulation/0-get_list_students.js
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,19 @@
 export default function getListStudents() {
-  const students = [
-    [1, 'Guillaume', 'San Francisco'],
-    [2, 'James', 'Columbia'],
-    [5, 'Serena', 'San Fransisco'],
+  return [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
   ];
-
-  const result = [];
-
-  students.forEach((student) => {
-    result.push(
-      {
-        id: student[0],
-        firstName: student[1],
-        location: student[2],
-      },
-    );
-  });
-
-  return result;
 }
