1f697b57f1d3fa71c1b33fbcc2d92b69ee2d2393
malek salem <malek.salem.14@gmail.com>
2021-09-21 21:49:21 +0100

    fix

--- 0x11-ES6_data_manipulation/6-set.js
+++ 0x11-ES6_data_manipulation/6-set.js
@@ -1,8 +1,3 @@
 export default function hasValuesFromArray(set, array) {
-  for (const val of array) {
-    if (!set.has(val)) {
-      return false;
-    }
-  }
-  return true;
+  return array.every((element) => set.has(element));
 }
