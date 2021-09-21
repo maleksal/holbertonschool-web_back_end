fabbb118aeea64af41b7f0a225f0fe02bcab9ac3
malek salem <malek.salem.14@gmail.com>
2021-09-21 21:51:28 +0100

    add task

--- 0x11-ES6_data_manipulation/6-set.js
+++ 0x11-ES6_data_manipulation/6-set.js
@@ -1,3 +1,3 @@
-export default function hasValuesFromArray(set, array) {
-  return array.every((element) => set.has(element));
+export default function setFromArray(Numbers) {
+  return new Set(Numbers);
 }
