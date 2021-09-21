64c4459784eb7adb69ab35b5473366e0c49e0741
malek salem <malek.salem.14@gmail.com>
2021-09-21 21:56:43 +0100

    add task

---
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+  let result = '';
+  if (!startString || !startString.length) {
+    return result;
+  }
+
+  set.forEach((i) => {
+    if (i && i.startsWith(startString)) {
+      result += `${i.slice(startString.length)}-`;
+    }
+  });
+  result = result.slice(0, result.length - 1);
+  return result;
+}
