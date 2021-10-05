b4d8607f92786f59d7beaa95c1339a6b30dbdf13
malek salem <malek.salem.14@gmail.com>
2021-10-06 00:13:11 +0100

    fix

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -2,19 +2,15 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
-const p = 1245;
 
-app.get('/', (request, response) => {
-  response.send('Hello Holberton School!');
-});
-
+app.listen(1245);
+app.get('/', (request, response) => {response.send('Hello Holberton School!');});
 app.get('/students', (request, response) => {
-  countStudents(process.argv[2]).then((result) => {
-    response.send(`This is the list of our students\n${result.join('\n')}`);
-  }).catch((error) => {
-    response.send(error.message);
-  });
+  countStudents(process.argv[2]).then((data) => {
+      response.end(`This is the list of our students\n${data}`);
+    }).catch((error) => {
+      response.end(`This is the list of our students\n${error.message}`);
+    });
 });
 
-app.listen(p);
 module.exports = app;
