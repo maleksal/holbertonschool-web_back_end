e607ae8e3d352d09d35ef6b3e9534dfbc9d725fd
malek salem <malek.salem.14@gmail.com>
2021-10-06 00:10:57 +0100

    add task

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,34 +1,21 @@
 const http = require('http');
-
-const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const DATABASE = args[0];
-
-const host = '127.0.0.1';
 const p = 1245;
 
 const app = http.createServer(async (request, response) => {
   response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-
-  const { url } = request;
-
-  if (url === '/') response.write('Hello Holberton School!');
-  else if (url === '/students') {
-    response.write('This is the list of our students\n');
-    try {
-      const allStudents = await countStudents(DATABASE);
-      response.end(`${allStudents.join('\n')}`);
-    } catch (error) {
-      response.end(error.message);
-    }
+  if (request.url === '/') response.end('Hello Holberton School!');
+  else if (request.url === '/students') {
+    await countStudents(process.argv[2]).then((success) => {
+      const output = `This is the list of our students\n${success}`;
+      response.end(output);
+    }).catch((err) => {
+      response.end(err.message);
+    });
   }
-  response.statusCode = 404;
-  response.end();
 });
 
-app.listen(p, host, () => {
-});
+app.listen(p);
 
 module.exports = app;
