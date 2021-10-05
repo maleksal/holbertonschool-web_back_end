3752caf842dd3915541380d741cffa6538745db8
malek salem <malek.salem.14@gmail.com>
2021-10-06 00:14:07 +0100

    fix eslint

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -4,13 +4,13 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 
 app.listen(1245);
-app.get('/', (request, response) => {response.send('Hello Holberton School!');});
+app.get('/', (request, response) => { response.send('Hello Holberton School!'); });
 app.get('/students', (request, response) => {
   countStudents(process.argv[2]).then((data) => {
-      response.end(`This is the list of our students\n${data}`);
-    }).catch((error) => {
-      response.end(`This is the list of our students\n${error.message}`);
-    });
+    response.end(`This is the list of our students\n${data}`);
+  }).catch((error) => {
+    response.end(`This is the list of our students\n${error.message}`);
+  });
 });
 
 module.exports = app;
