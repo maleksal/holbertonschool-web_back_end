77efba2281913cbedb0adcad4fad6d601a11f989
malek salem <malek.salem.14@gmail.com>
2021-10-05 23:21:05 +0100

    add task 2

---
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const countStudents = (path) => {
+  try {
+    let file = fs.readFileSync(path, { encoding: 'utf8' }).toString().split('\n');
+
+    file = file.slice(1, file.length - 1);
+
+    console.log(`Number of students: ${file.length}`);
+
+    const Classes = {};
+
+    file.forEach((row) => {
+      const line = row.split(',');
+
+      if (!Classes[line[3]]) {
+        Classes[line[3]] = [];
+      }
+      Classes[line[3]].push(line[0]);
+    });
+
+    Classes.forEach((cls) => {
+      if (cls) console.log(`Number of students in ${cls}: ${Classes[cls].length}. List: ${Classes[cls].join(', ')}`);
+    });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
+module.exports = countStudents;
