cd2f4ab236e15e74d40a5f52f1372423d4dea8fa
malek salem <malek.salem.14@gmail.com>
2021-10-06 00:16:09 +0100

    fix

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,21 +1,18 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const p = 1245;
-
-const app = http.createServer(async (request, response) => {
-  response.statusCode = 200;
-  if (request.url === '/') response.end('Hello Holberton School!');
-  else if (request.url === '/students') {
-    await countStudents(process.argv[2]).then((success) => {
-      const output = `This is the list of our students\n${success}`;
-      response.end(output);
-    }).catch((err) => {
-      response.end(err.message);
+const app = http.createServer((request, response) => {
+  if (request.url === '/') {
+    response.end('Hello Holberton School!');
+  }
+  if (request.url === '/students') {
+    countStudents(process.argv[2]).then((data) => {
+      response.end(`This is the list of our students\n${data}`);
+    }).catch((error) => {
+      response.end(`This is the list of our students\n${error.message}`);
     });
   }
 });
 
-app.listen(p);
-
+app.listen(1245);
 module.exports = app;
