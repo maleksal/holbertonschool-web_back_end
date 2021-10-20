043689c577d9ab4d77a094eab9c6972f408cb439
malek salem <malek.salem.14@gmail.com>
2021-10-20 14:36:18 +0100

    add new method to BasicAuth class

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,3 +8,12 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """BasicAuth class.
     """
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Returns Base64 of the Authorization header
+        """
+        if authorization_header is None\
+                or type(authorization_header) != str\
+                or not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split(" ")[1]
